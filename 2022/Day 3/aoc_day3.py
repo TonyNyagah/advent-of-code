a6c60ce7b0bf0a19b04f858b63034a954b7f5292
Antony Nyagah <tony.m.nyagah@gmail.com>
2022-12-03 12:53:29 +0000

    Dec 3, 2022, 3:53 PM

--- 2022/Day 3/aoc_day3.py
+++ 2022/Day 3/aoc_day3.py
@@ -35,16 +35,17 @@
 # if __name__ == "__main__":
 #     main()
 
-# def getData(txt_file) -> list:
-#     """Get data from a text file and remove new line characters."""
-#     with open(txt_file, "r") as f:
-#         data_list = f.read().splitlines()
-#     return data_list
+
+def getData(txt_file) -> list:
+    """Get data from a text file."""
+    with open(txt_file, "r") as f:
+        data_list = f.read().splitlines()
+    return data_list
 
 
-# if __name__ == "__main__":
-#     data = getData("test.txt")
+if __name__ == "__main__":
+    data = getData("test.txt")
 
-#     print(data)
+    print(data)
 
-#     print([len(x) for x in data])
+    print([len(x) for x in data])
