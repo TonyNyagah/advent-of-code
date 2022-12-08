cd1f1fa7a8abe74b7e60f3ee7590147b5f19b580
Antony Nyagah <tony.m.nyagah@gmail.com>
2022-12-08 16:48:57 +0300

    Completed day 7 of 2022 🙈

--- 2022/Day 7/aoc_day7.py
+++ 2022/Day 7/aoc_day7.py
@@ -1,3 +1,28 @@
-def getData(txt_file):
-    with open("input.txt", "r") as f:
-        return [line.strip() for line in f.readlines()]
+stack = []
+sizes = []
+
+
+def up():
+    sizes.append(stack.pop(-1))
+    if stack:
+        stack[-1] += sizes[-1]
+
+
+for line in open("aoc_day7_input.txt").readlines():
+    match line.strip().split():
+        case "$", "cd", "..":
+            up()
+        case "$", "cd", _:
+            stack.append(0)
+        case "$", _:
+            pass
+        case "dir", _:
+            pass
+        case size, _:
+            stack[-1] += int(size)
+
+while stack:
+    up()
+
+print(f"Part 1: {sum(s for s in sizes if s <= 100000)}")
+print(f"Part 2: {min(s for s in sizes if s >= (max(sizes) - 40000000))}")
