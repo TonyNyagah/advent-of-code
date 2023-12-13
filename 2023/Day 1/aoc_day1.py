0211ec8e751b439748d48a6354ed71f2f8ac8118
Antony Nyagah <tony.m.nyagah@gmail.com>
2023-12-13 17:24:26 +0300

    shu

--- 2023/Day 1/aoc_day1.py
+++ 2023/Day 1/aoc_day1.py
@@ -27,6 +27,88 @@ def part_one(data_list: list):
     return sum
 
 
+def part_two(data_list: list):
+    sum = 0
+    numeric_dict = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+    }
+
+
+# def main(do_tests: bool = False):
+#     with open("input.txt", "r") as file:
+#         lines: list = file.readlines()
+
+#     if do_tests:
+#         lines: list = [
+#             "two1nine",
+#             "eightwothree",
+#             "abcone2threexyz",
+#             "xtwone3four",
+#             "4nineeightseven2",
+#             "zoneight234",
+#             "7pqrstsixteen",
+#         ]
+
+#     numeric_dict = {
+#         # "zero": 0,
+#         "one": 1,
+#         "two": 2,
+#         "three": 3,
+#         "four": 4,
+#         "five": 5,
+#         "six": 6,
+#         "seven": 7,
+#         "eight": 8,
+#         "nine": 9,
+#         # "0": 0,
+#         "1": 1,
+#         "2": 2,
+#         "3": 3,
+#         "4": 4,
+#         "5": 5,
+#         "6": 6,
+#         "7": 7,
+#         "8": 8,
+#         "9": 9,
+#     }
+
+#     def extract_values(line: str) -> int:
+#         # Extract first and second digits from the line, return as int
+#         line: str = line.lower()  # make Lower case
+
+#         # Find indices
+#         number_indices: list = [
+#             (line.index(dig), val) for dig, val in numeric_dict.items() if dig in line
+#         ]
+
+#         f_dig: int = min(number_indices, key=lambda x: x[0])[1]  # Find smallest index
+#         l_dig: int = max(number_indices, key=lambda x: x[0])[1]  # Find largest index
+
+#         result = f_dig * 10 + l_dig
+#         return result
+
+#     lines: list = [extract_values(line) for line in lines]
+#     total = sum(lines)
+#     print(f"Sum is: {total}")
+
+
 if __name__ == "__main__":
     data = get_data("aoc_day1_input.txt")
-    print(f"The sum of all calibration values is: {part_one(data)}")
+    print(f"The sum of all calibration values for part one: {part_one(data)}")
