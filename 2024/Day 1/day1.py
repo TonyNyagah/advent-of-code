d711746407a84eb0b8a76a7c574b793ec07636ab
Antony Nyagah <tony.m.nyagah@gmail.com>
2024-12-01 21:21:39 +0300

    fix input filename in day 1 solution for 2024 to match new naming convention

--- 2024/Day 1/day1.py
+++ 2024/Day 1/day1.py
@@ -11,7 +11,7 @@ def calculate_similarity_score(left_list: list[int], right_list: list[int]) -> i
     return sum(a * right_list.count(a) for a in left_list)
 
 
-left_list, right_list = read_input("aoc_day1_input.txt")
+left_list, right_list = read_input("day1_input.txt")
 # Part 1
 print(calculate_total_distance(left_list, right_list))
 # Part 2
